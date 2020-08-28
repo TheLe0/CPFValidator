5fac6f35b070e20d513c10beab9ce7925c0d9a62
TheLe0 <leob.tosin@hotmail.com>
2020-08-27 21:08:49 -0300

    First Commit

---
+++ CPFValidator/CPFValidator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPFValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
